6ada4bb24492a210caa577d4552d0a802d93269d
Thomas Thorin <thomas.thorin@gmail.com>
2022-09-06 13:28:33 +0200

    Add project files.

---
+++ BookStore/ClientApp/src/atoms/weatherState.js
@@ -0,0 +1,8 @@
+import { atom } from "recoil";
+
+const weatherState =atom({
+	key:'weatherState',
+	default: null
+})
+
+export default weatherState
---
+++ BookStore/Program.cs
@@ -0,0 +1,42 @@
+// -----------------------------------------------------------------------------------------------
+//  Program.cs by Thomas Thorin, Copyright (C) 2022.
+//  Published under GNU General Public License v3 (GPL-3)
+// -----------------------------------------------------------------------------------------------
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html"); ;
+
+app.Run();
