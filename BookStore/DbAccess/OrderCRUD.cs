7909ae7ac6320a678b915953b7c29a8412613bc1
ahmed.salah.adam@outlook.com <ahmed.salah.adam@outlook.com>
2022-09-07 23:25:48 +0200

    Created simple begining of OrderCrud

---
+++ BookStore/DbAccess/OrderCRUD.cs
@@ -0,0 +1,37 @@
+﻿using BookStore.Models;
+using MongoDB.Driver;
+
+namespace BookStore.DbAccess
+{
+    public class OrderCRUD
+    {
+		private IMongoCollection<Order> orders;
+		public OrderCRUD(MongoDbAccess db)
+		{
+			orders = db.OrdersCollection;
+		}
+
+		public async Task<bool> CreateOrder(Order order)
+		{
+			await orders.InsertOneAsync(order);
+			var result = !String.IsNullOrWhiteSpace(order.CustomerId);
+			return result;
+		}
+		public async Task<List<Order>> GetAllOrders()
+		{
+			var resp = (await orders.FindAsync(_ => true)).ToList();
+			return resp;
+		}
+
+		public async Task<bool> UpdateOrders()
+        {
+			throw new NotImplementedException();
+        }
+
+		public async Task<bool> DeleteOrders()
+        {
+			throw new NotImplementedException();
+        }
+	}
+}
+
---
+++ BookStore/Models/Order.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace BookStore.Models
+{
+    public class Order
+    {
+
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string CustomerId { get; set; } = "";
+        public string Date { get; set; } = "";
+        public string OrderSum { get; set; } = "";
+        public string[] books { get; set; } = new string[] {};
+
+//        customerNumber:
+// ordernumber:
+// date:
+// beställare:
+// Order sum(total + frakt?)
+
+//books[book object array]:
+//  [
+//pippi(1)
+
+//emil(2)
+
+//lejonhjärta
+//]
+    }
+}
