c5ab570910de8c7808f407ea379210f96da09c13
ahmed.salah.adam@outlook.com <ahmed.salah.adam@outlook.com>
2022-09-08 22:59:40 +0200

    Edited Order Model

--- BookStore/Models/Order.cs
+++ BookStore/Models/Order.cs
@@ -9,24 +9,12 @@ namespace BookStore.Models
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public string Id { get; set; } = "";
         public string CustomerId { get; set; } 
+        public string CustomerName { get; set; }
+        public int Ordernumber { get; set; } = 0;
         public string Date { get; set; } = "";
         public decimal OrderSum { get; set; } = 0;
         public decimal VAT { get; set; } = 0;
         public string[] books { get; set; } = new string[] {};
 
-//        customerNumber:
-// ordernumber:
-// date:
-// beställare:
-// Order sum(total + frakt?)
-
-//books[book object array]:
-//  [
-//pippi(1)
-
-//emil(2)
-
-//lejonhjärta
-//]
     }
 }
