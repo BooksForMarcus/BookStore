3c854c09eca583f0cb5fa076329a249debb8d616
Cecilia Nilsson <cecilia.o.nilsson@live.se>
2022-09-08 09:24:17 +0200

    Added a basic framework for landingpage

--- BookStore/ClientApp/src/App.jsx
+++ BookStore/ClientApp/src/App.jsx
@@ -5,48 +5,112 @@ import { useRecoilState } from "recoil";
 import weatherState from "./atoms/weatherState";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [weather, setWeather] = useRecoilState(weatherState);
+    const [count, setCount] = useState(0);
+    const [weather, setWeather] = useRecoilState(weatherState);
 
-  const getCustomers = async () => {
-    const resp = await fetch("/api/customer");
-    const json = await resp.json();
-    console.log(json);
-    setWeather(json);
-  };
+    const getWeather = async () => {
+        const resp = await fetch("/api/weatherforecast");
+        const json = await resp.json();
+        console.log(json);
+        setWeather(json);
+    };
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={getCustomers}>Fetch customers now.</button>
-        {weather === null ? (
-          <p>Nothing fetched yet</p>
-        ) : (
-          weather.map((w, i) => {
-            return <p key={i}>{w.name}</p>;
-          })
-        )}
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header></header>
+            <main>
+                <div className="side"></div>
+                <div>
+                    <div className="card">
+                        <h3>Top 5 Nyheter</h3>
+                        {/*{weather === null ? (*/}
+                        {/*    <p>Boktitel</p>*/}
+                        {/*) : (*/}
+                        {/*    weather.map((w, i) => {*/}
+                        {/*        return*/}
+                        {/*        <div className="card-product">*/}
+                        {/*            <div className="book-image"></div>*/}
+                        {/*                <p key={i}>{w.name}</p>*/}
+                        {/*            </div>;*/}
+                        {/*    })*/}
+                        {/*)}*/}
+                        <div className="card-product">
+                            <div className="book-image"></div>
+                            <p>Boktitel</p>
+                        </div>
+                        <div className="card-product">
+                            <div className="book-image"></div>
+                            <p>Boktitel</p>
+                        </div>
+                        <div className="card-product">
+                            <div className="book-image"></div>
+                            <p>Boktitel</p>
+                        </div>
+                        <div className="card-product">
+                            <div className="book-image"></div>
+                            <p>Boktitel</p>
+                        </div>
+                        <div className="card-product">
+                            <div className="book-image"></div>
+                            <p>Boktitel</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <h3>Top 5 Begagnat</h3>
+                        <div className="card-product">
+                            <div className="book-image"></div>
+                            <p>Boktitel</p>
+                        </div>
+                        <div className="card-product">
+                            <div className="book-image"></div>
+                            <p>Boktitel</p>
+                        </div>
+                        <div className="card-product">
+                            <div className="book-image"></div>
+                            <p>Boktitel</p>
+                        </div>
+                        <div className="card-product">
+                            <div className="book-image"></div>
+                            <p>Boktitel</p>
+                        </div>
+                        <div className="card-product">
+                            <div className="book-image"></div>
+                            <p>Boktitel</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <h3>Hitta något nytt!</h3>
+                        <div className="card-product">
+                            <div className="book-image"></div>
+                            <p>Boktitel</p>
+                        </div>
+                        <div className="card-product">
+                            <div className="book-image"></div>
+                            <p>Boktitel</p>
+                        </div>
+                        <div className="card-product">
+                            <div className="book-image"></div>
+                            <p>Boktitel</p>
+                        </div>
+                        <div className="card-product">
+                            <div className="book-image"></div>
+                            <p>Boktitel</p>
+                        </div>
+                        <div className="card-product">
+                            <div className="book-image"></div>
+                            <p>Boktitel</p>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <footer>
+                {/*<button onClick={() => setCount((count) => count + 1)}>*/}
+                {/*    count is {count}*/}
+                {/*</button>*/}
+                <button onClick={getWeather}>Fetch weather forecast now.</button>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
