dfdc035133c7bcb536e9ac87c91991e276bf8954
jovalnik <jovalnik@gmx.com>
2022-09-13 15:33:36 +0200

    spararigen

---
+++ BookStore/Controllers/CategoryController.cs
@@ -0,0 +1,60 @@
+﻿namespace BookStore.Controllers;
+
+using BookStore.DbAccess;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+//using System.Web.Http;
+
+[Route("api/[controller]")]
+//[Route("api/[controller]")]
+[ApiController]
+public class CategoryController : ControllerBase
+{
+    private readonly CategoryCrud _categoryCrud;
+
+    public CategoryController(CategoryCrud categoryCrud) =>
+        _categoryCrud = categoryCrud;
+
+
+    // GET: api/<CategoryController>
+    [HttpGet]
+    public async Task<IEnumerable<Category>> Get()
+    {
+        return await _categoryCrud.GetAllCategories();
+    }
+
+    //[Route("api/[controller]/one/{id:guid}/")]
+    //[Route("{id:guid}")]
+    //public async Task<Category> Get(Guid id)
+    //{
+    //    return await _categoryCrud.GetCategory(id);
+    //}
+
+
+
+
+    [HttpPost]
+    public async Task<IActionResult> Post(Category category)
+    {
+
+        var result = await _categoryCrud.CreateCategory(category);
+        if (result) return Ok();
+        else return BadRequest();
+    }
+
+
+
+
+
+
+
+    public async Task<IActionResult> Delete(Category category)
+    {
+        var result = await _categoryCrud.DeleteCategory(category);
+        if (result) return Ok();
+        return BadRequest();
+    }
+
+
+}
+
