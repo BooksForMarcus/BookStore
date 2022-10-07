98ee1b15fb05c3e9865d3f31d9db0f941a132ce8
ahmed.salah.adam@outlook.com <ahmed.salah.adam@outlook.com>
2022-10-07 08:55:22 +0200

    add and decrease button to cart + some styling

--- BookStore/ClientApp/src/views/CartView/CartListItem.jsx
+++ BookStore/ClientApp/src/views/CartView/CartListItem.jsx
@@ -1,8 +1,12 @@
+import { useRecoilState } from 'recoil';
 import logo from '../../assets/boklogo.png'
 import cartState from '../../atoms/cartState';
 import './CartView.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
-const CarListItem = ({book,decreaseInCart, removeFromCart}) => {
+const CarListItem = ({book,decreaseInCart, removeFromCart, addToCart}) => {
+  const cart = useRecoilState(cartState);
 	return (
 		<div className='cartView-container'>
       <div className='cartView-info-l'>
@@ -21,7 +25,6 @@ const CarListItem = ({book,decreaseInCart, removeFromCart}) => {
                             )}
         <span className='cartView-item-details'>{book.title}</span>
         <span className='cartView-item-details'>á pris: {book.price}</span>
-        <span className='cartView-item-details'>Antal i varukorg: {book.numInstock}</span>
         <span className='cartView-item-details'>Summa: {book.price * book.numInstock}</span>
         {book.soldById !== "store" ? (
                     <div className="order-image"> Säljs begangnad av {book.soldById}</div>
@@ -35,7 +38,12 @@ const CarListItem = ({book,decreaseInCart, removeFromCart}) => {
                     </div> )}
         {book.numInstock >= 2 ? <button className='cart-item-button' onClick={()=>decreaseInCart(book)}>-</button>
          : <button className='cart-item-button' disabled>-</button> }
-        <button className='cart-item-button' onClick={()=>removeFromCart(book)}>Ta bort från varukorg</button>
+         <span className='cartView-item-details-amount'>{book.numInstock}</span>
+         <button className='cart-item-button' onClick={()=> addToCart(book)} 
+         disabled={cart.some((cartBook) => cartBook.id === book.id) && cart.find((cartBook)=> cartBook.id === book.id).numInstock >= book.numInstock
+         || book.numInstock === 0}>
+         + </button>
+        <button className='cart-item-remove-button' onClick={()=>removeFromCart(book)}><FontAwesomeIcon icon={faTrashCan}/></button>
       </div>
     </div>
 	  );
--- BookStore/ClientApp/src/views/CartView/CartView.jsx
+++ BookStore/ClientApp/src/views/CartView/CartView.jsx
@@ -36,6 +36,21 @@ function CartView() {
 	}
   };
 
+  const addToCart = (book) => {
+    let cartUpdate = [];
+    if (!cart.some((cartBook) => cartBook.id === book.id)) {
+      cartUpdate = [...cart, { ...book, numInstock: 1 }];
+    } else {
+      cartUpdate = cart.map((cartBook) => {
+        return cartBook.id === book.id
+          ? { ...cartBook, numInstock: cartBook.numInstock + 1 }
+          : cartBook;
+      });
+    }
+    setCart(cartUpdate);
+    localStorage.setItem("cart", JSON.stringify(cartUpdate));
+  };
+
   const removeFromCart = (bookToRemove) => {
     console.log("in decreaseInCart, bookToDecrease is: ", bookToRemove)
       const newCart = cart.filter((book) => book.id !== bookToRemove.id);
@@ -95,14 +110,18 @@ function CartView() {
       <div className='cart-main-wrapper'>
 	  {cart!==null || cart.length>0?
         cart.map((book) => (
-			<CarListItem key={"cart-"+book.id} book={book} decreaseInCart={decreaseInCart} removeFromCart={removeFromCart}/>
+			<CarListItem key={"cart-"+book.id} book={book} decreaseInCart={decreaseInCart} removeFromCart={removeFromCart} addToCart={addToCart}/>
         )): <div>Cart is empty</div>}
         <div>
           {(cart!==null && cart.length>0) ?
-          <div className="order-info">
-           <span className="order-info-item"> Frakt: {newCart(cart).postage.toFixed(2)}</span>
-           <span className="order-info-item"> Summa varukorg (inkl. frakt): {newCart(cart).orderSum + newCart(cart).VAT}</span> 
-           <span className="order-info-item"> Varav moms: {newCart(cart).VAT.toFixed(2)}</span>
+          <div> 
+            <div className="order-info">
+           <span className="order-info-item"> Frakt: {newCart(cart).postage.toFixed(2)} kr</span> 
+           <span className="order-info-item"> Moms: {newCart(cart).VAT.toFixed(2)} kr</span>
+           </div>
+           <div className="order-info">
+           <span className="order-info-item"> Summa totalt: {newCart(cart).orderSum + newCart(cart).VAT} kr</span>
+           </div>
            </div>
             : null}
           {(cart!==null && cart.length>0) ? 
