797c143fc6431172ea6d8b3848618bfb38145b12
Thomas Thorin <thomas.thorin@gmail.com>
2022-10-12 15:42:17 +0200

    css fix and removed BookOperationModel

--- BookStore/Models/BookOperation.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BookStore.Models;
-
-public class BookOperation
-{
-    public string Email { get; set; } = "";
-    public string Password { get; set; } = "";
-    public Book Book { get; set; } = new Book();
-}
